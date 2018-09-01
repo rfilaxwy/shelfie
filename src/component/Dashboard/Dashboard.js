88c9425c9c97c11cd7c82c2403bcd5ba80ee463d
rfilaxwy <ryanfilaxwylie@gmail.com>
2018-09-01 11:26:38 -0600

    Added components to app. Dashboard, Form, Header, Product

---
+++ src/component/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Dashboard extends React{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <div>DASHBOARD</div>
+        )
+    }
+}
---
+++ src/component/Form/Form.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Form extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <div>FORM</div>
+        )
+    }
+}
